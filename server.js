e86afd4c4302400bde3e927602de6bc85769ad6e
Chikanma <chikanmaonuegbu@gmail.com>
2022-07-19 09:25:31 -0600

    tt

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 
 //Connect to MongoDB
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "https://rentscape.herokuapp.com/",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
