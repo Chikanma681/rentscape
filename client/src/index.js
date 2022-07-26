c2eb65ee2363db7170699f28f0888b099ad9c4d5
Chikanma <chikanmaonuegbu@gmail.com>
2022-07-26 17:06:36 -0600

    revert to hashjrouter change

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 // Put any other imports below so that CSS from your
 // components takes precedence over default styles.
