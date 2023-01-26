ec822e61e519ecf1cd80c89c4e0f2b1405c82be0
Chikanma <chikanmaonuegbu@gmail.com>
2023-01-26 15:18:24 -0700

    remove disable-react-devtools

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
 import { BrowserRouter as Router } from "react-router-dom";
-import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+// import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 
 // Put any other imports below so that CSS from your
 // components takes precedence over default styles.
